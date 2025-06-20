f99cfbd8dd549d00067ae4356fd33fcd43041b8f
Anna Tolstoukhova <anna.tolstoukhova@mail.ru>
2025-06-20 15:50:42 +0300

    fix burger menu and write js code for menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
 'use strict';
 
 import "/src/sass/style.scss";
+
+
+    const burger = document.querySelector(".burger"), 
+          navMenu = document.querySelector(".burger__menu"),
+          burgerClose = document. querySelector(".burger__close");
+
+
+burger.addEventListener('click', () => {
+    navMenu.classList.add('burger__menu-active')
+    document.body.style.overflow = "hidden";
+});
+
+burgerClose.addEventListener('click', () => {
+    navMenu.classList.remove('burger__menu-active')
+    document.body.style.overflow = "";
+});
