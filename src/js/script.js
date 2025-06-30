e247a96d95b1add540f9fb0c22e3a98d857505cc
Anna Tolstoukhova <anna.tolstoukhova@mail.ru>
2025-06-30 12:01:31 +0300

    add purecss module and style activities section

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
+import 'purecss/build/grids-min.css';
+import 'purecss/build/grids-responsive-min.css';
 import "/src/sass/style.scss";
 
 
-    const burger = document.querySelector(".burger"), 
-          navMenu = document.querySelector(".burger__menu"),
-          burgerClose = document. querySelector(".burger__close");
+const burger = document.querySelector(".burger"), 
+      navMenu = document.querySelector(".burger__menu"),
+      burgerClose = document. querySelector(".burger__close");
 
 
 burger.addEventListener('click', () => {
