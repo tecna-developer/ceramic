c34ed61b316109d6ab80029982c3af8026de7763
Anna Tolstoukhova <anna.tolstoukhova@mail.ru>
2025-07-01 15:42:26 +0300

    remove purecss module and add style files in libs directory

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,17 @@
-'use strict';
+"use strict";
 
-import 'purecss/build/grids-min.css';
-import 'purecss/build/grids-responsive-min.css';
 import "/src/sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+  navMenu = document.querySelector(".burger__menu"),
+  burgerClose = document.querySelector(".burger__close");
 
-const burger = document.querySelector(".burger"), 
-      navMenu = document.querySelector(".burger__menu"),
-      burgerClose = document. querySelector(".burger__close");
-
-
-burger.addEventListener('click', () => {
-    navMenu.classList.add('burger__menu-active')
-    document.body.style.overflow = "hidden";
+burger.addEventListener("click", () => {
+  navMenu.classList.add("burger__menu-active");
+  document.body.style.overflow = "hidden";
 });
 
-burgerClose.addEventListener('click', () => {
-    navMenu.classList.remove('burger__menu-active')
-    document.body.style.overflow = "";
+burgerClose.addEventListener("click", () => {
+  navMenu.classList.remove("burger__menu-active");
+  document.body.style.overflow = "";
 });
