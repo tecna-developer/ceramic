8d8ff9d56778a6acbff62839440a195b8b49c08b
Anna Tolstoukhova <anna.tolstoukhova@mail.ru>
2025-07-03 17:07:48 +0300

    add swiper.js and styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 "use strict";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "/src/sass/style.scss";
 
 const burger = document.querySelector(".burger"),
@@ -15,3 +20,33 @@ burgerClose.addEventListener("click", () => {
   navMenu.classList.remove("burger__menu-active");
   document.body.style.overflow = "";
 });
+
+try {
+  new Swiper(".swiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+    breakpoints: {
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 5,
+        mousewheel: true,
+        keyboard: true,
+      },
+      // when window width is >= 1920px
+      1920: {
+        spaceBetween: 35,
+      },
+    },
+
+    modules: [Navigation, Pagination],
+  });
+} catch (error) {}
