6cbe5fe6dbda3f4feefa8c8a57ce677a8e150b19
Anna Tolstoukhova <anna.tolstoukhova@mail.ru>
2025-08-03 22:24:35 +0300

    js:import justValidate

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
+import JustValidate from "just-validate";
 
 import "swiper/css";
 import "swiper/css/navigation";
